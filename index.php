5870881414de0b19b7689493681d4aa54308edfd
VladHilea <79455788+VladHilea@users.noreply.github.com>
2021-04-30 23:37:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="ro-RO" dir="ltr">
+
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="user-scalable=yes, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/vegas.min.css">
+	<link rel="shortcut icon" href="images/favicon.ico" />
+	
+
+</head>
+<body>
+
+<!-- start menu transparent -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="pull-right navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand page-scroll opacity-60" href="#top">Magreb</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      <ul class="nav navbar-nav navbar-right text-center ">
+        <li><a class="page-scroll" href="#about">Ce este Magreb?</a></li>
+        <li><a class="page-scroll" href="#program">Istorie</a></li>
+        <li><a class="page-scroll" href="#prieteni">Uniunea Magrebului Arab</a></li>
+        <li><a class="page-scroll" href="#social">Limba</a></li>
+        
+     
+    
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!-- sfarsit menu transparent --> 
+
+
+<section  class=" height-100" id="top">
+	<section class="opacity-black position-relative height-100">
+		<div class="container display-table height-100" >
+			<div class="row vertical-align" >
+				
+			</div>
+		</div>
+	</section>
+</section>
+ 
+
+
+</section>
+<section id="about" class="bg-black">
+<section class="bg-white">
+
+	<div class="container">
+		<div class="row text-center pb pt">
+			<h1><b>Ce este Magreb?</b></h1>
+		</div>
+	</div>
+</section>
+		<div class="container pb pt" >
+			<div class="row  text-center pb pt center-block font-white">
+				<div class="col-sm-6 ">
+  					<img class="img-responsive wow slideInUp " src="images/magreb4.png" alt="...">
+
+   				</div>
+   				<div class="col-sm-6 ">
+  					<h2>Prin Magreb  se înțeleg în primul rând cele trei țări nordafricane Tunisia, Algeria și Maroc, Egiptul, parțial și Libia și Mauritania, care, pe baza istoriei lor, au multe trăsături comune. În arabă prin Maghreb de regulă se ințelege Marocul, pentru că este cea mai vestica țară arabă.
+
+Opusul Maghreb-ului este Mashriq-ul, „Est“, „Răsărit”, derivat din verbul arab sharaqa: „a răsări”.</h2>
+
+   				</div>
+  					
+			
+		</div>
+</section>
+<section id="program">
+	<div class="container pt-50">
+		<div class="row text-center pt pb">
+			<div >
+				<h1 class="text-center text-font "><b>Istorie</b></h1>	
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+<section class="bg-colo-1 ">
+
+
+	<div class="container pb pt" >
+			<div class="row  text-center pb pt center-block">
+				<div class="col-sm-6 ">
+  					<img class="img-responsive wow slideInUp " src="images/magreb5.jpg" alt="...">
+
+   				</div>
+   				<div class="col-sm-6 ">
+  					<h4>Inițial, culturile în Maghreb au fost de natură pur africană. Cu extinderea agriculturii au ajuns în Egipt influențe orientale și odată cu extinderea culturii cardiale (sau impresso - neoliticul timpuriu în zona vest-mediteraneană) și în Maghreb. În Antichitate acesta a fost inițial sub influență feniciană. Cartagina, cel mai mare și puternic oraș fenician se afla în Tunisia. După Războaiele Punice întreaga regiune a fost înglobată Imperiului Roman. Cu extinderea religiei islamice în secolul al VII-lea, regiunea a intrat sub influență arabă, cu toate că până astăzi diverse minorități vorbesc limba autohtonă berberă. Influența seculară, imediat următoare, a Imperiului Otoman a durat pe toată perioada colonizării europene, în secolele al XIX-lea și al XX-lea. Granițele actuale ale țărilor membre se bazează parțial pe decizii otomane și, parțial, europene.</h4>
+
+   				</div>
+   			</div>
+  					
+			
+		</div>
+</section>
+<section class="opacity-black-2 font-white">
+	<div class="container pt-50">
+		<div class="row text-center pt pb">
+			<div class="col-sm-6">
+			
+				<h2> <b class="counter"> 5</b> <b ></b></h2>
+				<h3>Tari</h3>
+			</div>
+
+			<div class="col-sm-6">
+			
+				<h2><b class="counter">1 </b> <b> </b></h2>
+				<h3>Uniune</h3>
+			</div>
+			
+		</div>
+	</div>			
+</section>
+</section>
+</section>
+
+<section id="prieteni">
+	<div class="container pb pt">
+		<div class="row  text-center pb">
+			<div class="col-sm-12 pb-60"> <h1><b>Uniunea Magreb-ului Unit</b></h1></div>
+				<div class="row  text-center pb pt center-block">
+				<div class="col-sm-6 ">
+  					<img class="img-responsive wow slideInUp " src="images/magreb5.PNG" alt="...">
+  					<img style="	padding-top: 20px;" class="img-responsive wow slideInUp " src="images/magreb5.PNG" alt="...">
+
+   				</div>
+   				<div class="col-sm-6 ">
+  					<h4>Uniunea Magrebului Arab (UMA) (în arabă Ittihād al-Maghrib al-'Arabī, în franceză UMA - Union du Maghreb Arabe; în engleză AMU - Arab Maghreb Union) este o asociere regională de state din Africa de Nord, fondată la 17 februarie 1989. Uniunea este formată din Algeria, Libia, Mauritania, Maroc (fără Sahara Occidentală) și Tunisia, și își are sediul în Rabat, Maroc.
+
+Anterior creării, a avut loc o întâlnire a șefilor celor cinci state maghrebiene în iunie 1988 în Zeralda, Algeria.
+
+Scopurile uniunii sunt cooperarea economică, cooperare politico-culturală internă și externă. Numeroase proiecte regionale, care erau planificate de mult timp, cum ar fi extinderea conexiunilor transmaghrebiene a căilor ferate, șoselelor și conductelor, au putut fi continuate. Menținerea valorilor sufletești și morale ale Islamului și asigurarea identității arabe, printre altele și prin schimb pedagogic de profesori și studenți, instituții culturale și universitare comune.
+
+Problemele UMA la punerea în viață a proiectelor sunt prevederile legale diferite ale statelor membre, birocrația greoaie, schimburile comerciale reciproce scăzute (numai trei procente ale exporturilor totale) și lipsa mijloacelor financiare. Până astăzi nu s-a putut crea o uniune vamală.
+
+În 1991, toate statele membre au acceptat un plan de acțiune pentru integrarea economică a regiunii, care trebuia să conducă până la o uniune economică.
+
+Readucerea la viață a uniunii maghrebiene amenință să eșueze din cauză că normalizarea de lungă durată a relațiilor marocano-algeriene, încă puternic tensionate din cauza conflictului cu Sahara Occidentală, se lasă așteptată.
+
+Din punctul de vedere al Uniunii Europene și al cercurilor de elită din Maghreb, din cauza valurilor de refugiați din Melilla din vara anului 2005, uniunii statelor maghrebiene îi revine chiar o importanță actuală. Mai ales în Maroc există speranța că întâlnirea din 1995 de la Barcelona a țărilor membre UE și a multor țări mediteraneene și continuarea acesteia din perioada 27 - 29 noiembrie 2005 să dea noi impulsuri creării unei zone de comerț liber al țărilor sud și est mediteraneene.
+
+Schimburile comerciale între țările maghrebiene s-au menținut reduse: Algeria efectuează schimburi comerciale de doar 1,5 procente din comerțul său exterior cu țările UMA (peste 60 de procente cu Uniunea Europeană), Tunisia, ca țara cea mai orientată spre Maghreb numai șase procente (75 procente cu UE). (Date din anul 2000.).</h4>
+
+   				</div>
+   			</div>
+			</div>
+		</div>
+	</div>
+	</section>
+	
+<section id="social">
+	<div class="container  ">
+		<div class="row"> <div class="col-sm-12 text-center"> <h1> Limba</h1></div></div>
+		<div class="row  text-center pb pt center-block">
+				<h2>	Pe lângă limba arabă literară care este limba oficială în toate statele maghrebiene, în mod uzual, sunt folosite diverse dialecte arabe care sunt, în bună parte, reciproc inteligibile. Toate aceste dialecte au fost puternic influențate de dialectele berbere, iar mai târziu de limba franceză și, în unele zone, de spaniolă (nordul Marocului) și de italiană (Libia). Alături de dialectele arabe maghrebiene - ce formează un grup distinct în cadrul dialectelor arabe moderne - în foarte multe regiuni ale Maghreb-lui se vorbesc dialecte berbere. Limba malteză, limba oficială a Maltei, a fost inițial un dialect maghrebian, care a fost, însă, dezvoltat până la a fi o limbă standard modernă.
+
+În anul 2002[1], în Algeria, limba berberă (tamazight) a fost declarată limbă națională alături de limba arabă, care, însă, rămâne singura limbă oficială a statului[2].</h2>
+   			</div>
+	</div>
+</section>
+
+<section class="bg-black">
+	<div class="container">
+		<div class="row text-center pb pt">
+			<div class="col-sm-12">
+				<h4>Created by Vlad Hilea</h4>
+				<h4>Copyright @ All rights reserved to Vlad Hilea</h4>
+				<h5>Vegas Slider by Jay Salvat</h5>
+			</div>
+		</div>
+	</div>
+</section>
+
+<a href="#top" id="goto" class='page-scroll dn  fs-18'>
+<span class="fa-stack fa-lg hidden-xs" style='position:fixed; bottom:60px; right:60px;'>
+  <i class="fa fa-circle fa-stack-2x"></i>
+  <i class="fa bg-yellow-1 img-circle fa-arrow-up font-white fa-stack-1x fa-inverse"></i>
+</span>
+</a>
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+<script src="js/counterup.min.js"></script>
+ <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/typed.min.js"></script>
+<script src="js/vegas.min.js"></script>
+<script>  
+jQuery(window).scroll(function() {
+    if (jQuery(".navbar").offset().top > 50) {
+        jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+
+        } 
+    else
+        {
+        jQuery(".navbar-fixed-top").removeClass("nav-collapsed");
+
+        }
+});
+</script>
+<script>
+var width = $(window).width();
+if (width > 768) {jQuery(window).scroll(function() {
+    if (jQuery(".navbar").offset().top > 50) {
+        jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+
+        } 
+    else
+        {
+        jQuery(".navbar-fixed-top").removeClass("nav-collapsed");
+
+        }
+});
+}
+ else{
+ 	 jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+ }
+
+
+ 
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+     jQuery('.page-scroll').bind('click', function(e) {
+            e.preventDefault();
+            jQuery('html,body').animate({scrollTop: jQuery(this.hash).offset().top - 50});                                                         
+               });
+       $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 1250) {  
+       
+        $("#goto").removeClass("dn");
+    }
+    else{
+ $("#goto").addClass("dn");
+    }
+}); 
+</script>
+<script>
+    jQuery(document).ready(function( $ ) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    });
+</script>
+<script >
+	$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+</script>
+<script >
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    center: true;
+    responsive:{
+        0:{
+            items:
+        }
+    }
+});
+</script>
+<script>
+	var typed = new Typed("#typed", {
+  strings: ["#RESPEEEECT ", "#PUTEREEEE ", "#MUIELACAI ", "#COLOSQUAD "],
+  typeSpeed: 50,
+  smartBackspace:true,
+    loop: true,
+  loopCount: Infinity,
+   backSpeed: 50,
+    backDelay: 800,
+
+});
+</script>
+<script>
+    $("#top").vegas({
+    slides: [
+        { src: "images/maghreb.jpg" },
+        { src: "images/magreb2.png" }
+    ],
+    animation: 'kenburns',
+    delay:8000
+});
+</script>
+</body>
+</html>
+
+
